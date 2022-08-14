44c1351055a5b9ec1e77632fa5a50d04e373a652
leongck1 <38928795+leongck1@users.noreply.github.com>
2022-08-14 22:47:18 +0800

    Update Image Labeler.py

--- 2022/Round D/Image Labeler.py
+++ 2022/Round D/Image Labeler.py
@@ -1,11 +1,10 @@
 # Time Complexity = O(NlogN + M) where NlogN is due to sorting of A and M is due to the for loop of M - 1.
 
-def median(data):
-    if len(data) % 2 == 0:
-        index_1 = int((len(data) - 1) // 2)
-        index_2 = index_1 + 1
-        median = (data[index_1] + data[index_2]) / 2
-    elif len(data) % 2 == 1:
+def median(data: list):
+    if len(data) % 2 == 0:  # Check if the length of the list is even)
+        index = int((len(data) - 1) // 2)
+        median = (data[index] + data[index + 1]) / 2
+    else:  # If the length of the list is odd
         index = int((len(data) - 1) / 2)
         median = data[index]
     return median
@@ -13,8 +12,7 @@ def median(data):
 T = int(input())
 for i in range(1, T + 1):
     N, M = list(map(int, input().split()))
-    A = list(map(int, input().split()))
-    A = sorted(A, reverse=True)
+    A = sorted(list(map(int, input().split())), reverse=True)
     result = 0
     for k in range(M - 1):
         result += A[k]
