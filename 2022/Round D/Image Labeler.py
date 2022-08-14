c159ce76d9897e2c2495c70af0ce8e5943d4afb4
leongck1 <38928795+leongck1@users.noreply.github.com>
2022-08-14 22:25:11 +0800

    Create Image Labeler.py

---
+++ 2022/Round D/Image Labeler.py
@@ -0,0 +1,21 @@
+def median(data):
+    if len(data) % 2 == 0:
+        index_1 = int((len(data) - 1) // 2)
+        index_2 = index_1 + 1
+        median = (data[index_1] + data[index_2]) / 2
+    elif len(data) % 2 == 1:
+        index = int((len(data) - 1) / 2)
+        median = data[index]
+    return median
+
+
+T = int(input())
+for i in range(1, T + 1):
+    N, M = list(map(int, input().split()))
+    A = list(map(int, input().split()))
+    A = sorted(A, reverse=True)
+    result = 0
+    for k in range(M - 1):
+        result += A[k]
+    result += median(A[M - 1:])
+    print(f"Case #{i}: {result}")
